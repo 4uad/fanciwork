7ab181484d08e5f367c2a802f7abcbfd05251c06
4uad <dmamadrid@gmail.com>
2021-03-04 16:49:24 +0100

    add proptype tests

--- client/src/components/Results/Job/Job.test.js
+++ client/src/components/Results/Job/Job.test.js
@@ -14,22 +14,72 @@ describe('Job component', () => {
 
     let job;
 
-    it('has the expected prop-types', () => {
-        const expectedProps = {
-            title: 'title',
-            url: 'url',
-            tags: ['tag1', 'tag2'],
-            logo: 'logourl',
-            company: 'company'
-        };
-
-        const propsErr = checkProps(Job, expectedProps);
-        expect(propsErr).toBeUndefined();
-    });
-
     describe('With tags', () => {
         beforeEach(() => {
             job = init({tags: testTags});
+        });
+
+        describe('Prop-types', () => {
+            it('nothing missing', () => {
+                const expectedProps = {
+                    title: 'title',
+                    url: 'url',
+                    tags: ['tag1', 'tag2'],
+                    logo: 'logourl',
+                    company: 'company'
+                };
+        
+                const propsErr = checkProps(Job, expectedProps);
+                expect(propsErr).toBeUndefined();
+            });
+
+            it('title missing', () => {
+                const expectedProps = {
+                    url: 'url',
+                    tags: ['tag1', 'tag2'],
+                    logo: 'logourl',
+                    company: 'company'
+                };
+        
+                const propsErr = checkProps(Job, expectedProps);
+                expect(propsErr).toBeUndefined();
+            });
+
+            it('url missing', () => {
+                const expectedProps = {
+                    title: 'title',
+                    tags: ['tag1', 'tag2'],
+                    logo: 'logourl',
+                    company: 'company'
+                };
+        
+                const propsErr = checkProps(Job, expectedProps);
+                expect(propsErr).toBeUndefined();
+            });
+
+            it('logo missing', () => {
+                const expectedProps = {
+                    title: 'title',
+                    url: 'url',
+                    tags: ['tag1', 'tag2'],
+                    company: 'company'
+                };
+        
+                const propsErr = checkProps(Job, expectedProps);
+                expect(propsErr).toBeUndefined();
+            });
+
+            it('company missing', () => {
+                const expectedProps = {
+                    title: 'title',
+                    url: 'url',
+                    tags: ['tag1', 'tag2'],
+                    logo: 'logourl'
+                };
+        
+                const propsErr = checkProps(Job, expectedProps);
+                expect(propsErr).toBeUndefined();
+            });
         });
 
         it('renders without errors', () => {
@@ -48,6 +98,69 @@ describe('Job component', () => {
     describe('Without tags', () => {
         beforeEach(() => {
             job = init({tags: []});
+        });
+
+        describe('Prop-types', () => {
+            it('nothing missing', () => {
+                const expectedProps = {
+                    title: 'title',
+                    url: 'url',
+                    tags: [],
+                    logo: 'logourl',
+                    company: 'company'
+                };
+        
+                const propsErr = checkProps(Job, expectedProps);
+                expect(propsErr).toBeUndefined();
+            });
+
+            it('title missing', () => {
+                const expectedProps = {
+                    url: 'url',
+                    tags: [],
+                    logo: 'logourl',
+                    company: 'company'
+                };
+        
+                const propsErr = checkProps(Job, expectedProps);
+                expect(propsErr).toBeUndefined();
+            });
+
+            it('url missing', () => {
+                const expectedProps = {
+                    title: 'title',
+                    tags: [],
+                    logo: 'logourl',
+                    company: 'company'
+                };
+        
+                const propsErr = checkProps(Job, expectedProps);
+                expect(propsErr).toBeUndefined();
+            });
+
+            it('logo missing', () => {
+                const expectedProps = {
+                    title: 'title',
+                    url: 'url',
+                    tags: [],
+                    company: 'company'
+                };
+        
+                const propsErr = checkProps(Job, expectedProps);
+                expect(propsErr).toBeUndefined();
+            });
+
+            it('company missing', () => {
+                const expectedProps = {
+                    title: 'title',
+                    url: 'url',
+                    tags: [],
+                    logo: 'logourl'
+                };
+        
+                const propsErr = checkProps(Job, expectedProps);
+                expect(propsErr).toBeUndefined();
+            });
         });
 
         it('renders without errors', () => {
